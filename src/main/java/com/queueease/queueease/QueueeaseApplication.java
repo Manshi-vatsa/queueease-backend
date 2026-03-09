fcffa55af06d984f48d77feb1e5f2d52b394c3c9
Manshi-vatsa <manshi.22190503030@cuj.ac.in>
2026-03-09 22:27:06 +0530

    Added gitignore

---
+++ src/main/java/com/queueease/queueease/QueueeaseApplication.java
@@ -0,0 +1,13 @@
+package com.queueease.queueease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueeaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/queueease/queueease/QueueeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queueease.queueease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
